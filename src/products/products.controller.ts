ee1708c7f6775fdb0cce4cd13e2ae4e19a7e8429
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2024-07-18 20:01:02 -0600

    create validate product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -54,4 +54,11 @@ export class ProductsController {
   ) {
     return this.productsService.remove(id);
   }
+
+  @MessagePattern({ cmd: 'validate_products' })
+  validateProducts(
+    @Payload() ids: number[]
+  ) {
+    return this.productsService.validateProducts(ids);
+  }
 }
